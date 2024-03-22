24d1c966f7cbf1cc69f8e1a75493a884267842d0
Ruchi Badkur <94377483+RuchiBadkur@users.noreply.github.com>
2024-03-22 23:23:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+const body = document.querySelector("body");
+const main = document.createElement("main");
+const input = document.createElement("input");
+const table = document.createElement("table");
+const arr = ["C", "/", "*", "<-", 7, 8, 9, "-", 4, 5, 6, "+", 1, 2, 3, ".", "(", 0, ")", "="];
+let item = 0;
+for(let row = 1; row <= 5; row++){
+	const tr = document.createElement("tr");
+	for(let col = 1; col <= 4; col++){
+		var btn = document.createElement("button");
+		btn.innerText = arr[item];
+		console.log(btn.innerText);
+		item++;
+		tr.append(btn);
+	}
+		table.append(tr);
+}
+main.append(input);
+main.append(table);
+body.append(main);
